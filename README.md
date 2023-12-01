d6051d74249ba935736e87232badfb8ea30e1413
Willem-Jaap <willemjaapk@gmail.com>
2023-12-01 23:17:06 +0100

    feat: add day 1, part 2, add readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Advent of Code 2023
+
+```bash
+pnpm run dev day-[number]/index.ts
+```
--- day-1/index.ts
+++ day-1/index.ts
@@ -2,7 +2,27 @@ const input = await Deno.readTextFile("./day-1/input.txt");
 
 const lines = input.split("\n");
 
-const numbers = lines.map((line: string) => {
+const map = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+};
+
+const correctLines = lines.map((line) => {
+    Object.entries(map).forEach(([number, string]) => {
+        line = line.replaceAll(string, string + number + string);
+    });
+
+    return line;
+});
+
+const numbers = correctLines.map((line: string) => {
     const numbers = line.match(/\d/g);
     if (!numbers || numbers.length < 1) return 0;
 
